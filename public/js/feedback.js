0e7ec7b968e3cd039dd01e990f307b20a95fecb0
Chris-Humphrey <clhumphrey21@gmail.com>
2019-06-06 12:09:24 -0500

    made feedback look pretty

--- public/js/feedback.js
+++ public/js/feedback.js
@@ -12,16 +12,20 @@ $(function() {
             comment: $('#comment').val()
         }, displayData);
     });
-    
+
     function displayData(feedbackArray) {
         var html = "";
     
         $.each(feedbackArray, (key, feedbackObj) => {
-            html += `<div class="border border-secondary p-2 mb-2">`
-            html += `<p><b>Name<br></b> ${feedbackObj.name}</p>`;
-            html += `<p><b>Email<br></b> ${feedbackObj.email}</p>`;
-            html += `<p><b>Comments<br></b> ${feedbackObj.comment}</p>`;
-            html += `</div>`
+            html += `<div class="border border-secondary p-2 mb-2">`;
+            html += `<div class="d-flex d-inline-flex">`;
+            html += `<p class="mr-4"><b>Name: </b>${feedbackObj.name}</p>`;
+            html += `<p><b>Email: </b> ${feedbackObj.email}</p>`;
+            html += `</div>`;
+            html += `<div class="d-flex flex-column">`;
+            html += `<p class="flex-column"><b>Comments<br></b> ${feedbackObj.comment}</p>`;
+            html += `</div>`;
+            html += `</div>`;
         });
     
         $('#feedback').html(html);
