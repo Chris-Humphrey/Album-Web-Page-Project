9ffd6636827a294e87c7baad425fba215c241671
Chris-Humphrey <clhumphrey21@gmail.com>
2019-06-06 11:49:53 -0500

    update

--- public/js/feedback.js
+++ public/js/feedback.js
@@ -12,14 +12,15 @@ $(function() {
             comment: $('#comment').val()
         }, displayData);
     });
+    
     function displayData(feedbackArray) {
         var html = "";
     
         $.each(feedbackArray, (key, feedbackObj) => {
-            html += `<div class="border border-secondary mb-2">`
-            html += `<p><b>Name:</b> ${feedbackObj.name}</p>`;
-            html += `<p><b>Email:</b> ${feedbackObj.email}</p>`;
-            html += `<p><b>Comments:</b> ${feedbackObj.comment}</p>`;
+            html += `<div class="border border-secondary p-2 mb-2">`
+            html += `<p><b>Name<br></b> ${feedbackObj.name}</p>`;
+            html += `<p><b>Email<br></b> ${feedbackObj.email}</p>`;
+            html += `<p><b>Comments<br></b> ${feedbackObj.comment}</p>`;
             html += `</div>`
         });
     
